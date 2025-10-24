84bfc27f58dadf1d34613795d4d94fe2e39acbe4
Carlos Montoya <98224572+CarlosMontoya17@users.noreply.github.com>
2025-10-23 23:39:57 -0600

    ⚡ Claims Service
    Actions methods:
    - Delete record
    - Add new record
    - Update
    
    Transactions methods:
    - Delete record
    - Add new record
    - Update

--- src/app/services/cliamsService.ts
+++ src/app/services/cliamsService.ts
@@ -1,5 +1,7 @@
 // services/claimsService.ts
 import apiClient from "../helpers/apiClient";
+import { ClaimsActionsCreateModel } from "../pages/claims/models/ClaimsActionsCreateModel";
+import { ClaimsTransactionsCreateModel } from "../pages/claims/models/ClaimsTransactionsCreateModel";
 
 export const createClaims = async (
   file: File,
@@ -33,6 +35,50 @@ export const getActionsClaims = async (user_id?: string) => {
   }
 };
 
+export const getActionsClaimsById = async (id: number) => {
+  try {
+    const response = await apiClient.get(`/claims/claim-actions/${id}/`);
+    return response.data;
+  }
+  catch (error) {
+    console.error('Error fetching claim action:', error);
+    throw error;
+  }
+};
+
+export const createActionsClaim = async (data: ClaimsActionsCreateModel) => {
+  try {
+    const response = await apiClient.post(`/claims/claim-actions/`, data);
+    return response.data;
+  }
+  catch (error) {
+    console.error('Error fetching claim transaction:', error);
+    throw error;
+  }
+}
+
+export const updateActionsClaim = async (id: number, data: ClaimsActionsCreateModel) => {
+  try {
+    const response = await apiClient.put(`/claims/claim-actions/${id}/`, data);
+    return response.data;
+  }
+  catch (error) {
+    console.error('Error fetching claim transaction:', error);
+    throw error;
+  }
+}
+
+export const deleteActionsClaim = async (id: number) => {
+  try {
+    const response = await apiClient.delete(`/claims/claim-actions/${id}/`);
+    return response.data;
+  }
+  catch (error) {
+    console.error('Error fetching claim action:', error);
+    throw error;
+  }
+}
+
 export const getTransactionsClaims = async (page: number, user_id?: string) => {
   try {
     const user = user_id ? `&user_id=${Number(user_id)}` : '';
@@ -44,6 +90,51 @@ export const getTransactionsClaims = async (page: number, user_id?: string) => {
     throw error;
   }
 };
+
+
+export const getTransactionsClaimById = async (id: number) => {
+  try {
+    const response = await apiClient.get(`/claims/claim-transactions/${id}/`);
+    return response.data;
+  }
+  catch (error) {
+    console.error('Error fetching claim action:', error);
+    throw error;
+  }
+};
+
+export const createTransactionsClaim = async (data: ClaimsTransactionsCreateModel) => {
+  try {
+    const response = await apiClient.post(`/claims/claim-transactions/`, data);
+    return response.data;
+  }
+  catch (error) {
+    console.error('Error fetching claim transaction:', error);
+    throw error;
+  }
+}
+
+export const updateTransactionsClaim = async (id: number, data: ClaimsTransactionsCreateModel) => {
+  try {
+    const response = await apiClient.put(`/claims/claim-transactions/${id}/`, data);
+    return response.data;
+  }
+  catch (error) {
+    console.error('Error fetching claim transaction:', error);
+    throw error;
+  }
+}
+
+export const deleteTransactionsClaim = async (id: number) => {
+  try {
+    const response = await apiClient.delete(`/claims/claim-transactions/${id}/`);
+    return response.data;
+  }
+  catch (error) {
+    console.error('Error fetching claim transaction:', error);
+    throw error;
+  }
+}
 
 export const getTransactionsLogs = async (guid: string) => {
   try {
