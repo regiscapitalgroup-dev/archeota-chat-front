8637e961aade92e8daa5ca0ef65ffa72b239e0ec
Carlos Montoya <98224572+CarlosMontoya17@users.noreply.github.com>
2025-10-30 15:40:45 -0600

    🩹 Asset form edit
    * Load asset data by id
    * Go to the list page when the request is successful
    * Unnecessary components were deleted

--- src/app/pages/assets/AssetsPage.tsx
+++ src/app/pages/assets/AssetsPage.tsx
@@ -1,20 +1,17 @@
 import React, { lazy } from 'react';
 import { Route, Switch, useRouteMatch } from 'react-router-dom';
-import AssetsForm from './components/AssetsForm';
-import ChatBot from '../chat/components/ChatBot';
 import Chat from '../chat/features/chat/Chat';
+import AssetsForm from './components/AssetsForm';
 
 const AssetsPage: React.FC = () => {
   const { path } = useRouteMatch();
   const Assets = lazy(() => import('./features/assets/Assets'));
   const AssetsForms = lazy(() => import('./features/assets/AssetsForms'));
   const AssetsDetails = lazy(() => import('./features/assets/AssetsDetails'));
-  // const ChatBot = lazy(() => import('../chat/features/chat/Chat'));
   
   return (
     <Switch>
       <Route exact path={`${path}`} component={Assets}/>
-      
       <Route path={`${path}/new`} component={AssetsForm}/>
       <Route path={`${path}/edit/:id`} component={AssetsForms}/>
       <Route path={`${path}/detail/:id`} component={AssetsDetails}/>
