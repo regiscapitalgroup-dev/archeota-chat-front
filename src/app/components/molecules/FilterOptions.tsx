daed979031de5818e3d07465568884ed82a17168
Carlos Montoya <98224572+CarlosMontoya17@users.noreply.github.com>
2025-11-21 22:43:13 -0600

    🩹 Fix filter options
    * Add an accordion-group to handle popup height
    * split text and date from string options

--- src/app/components/molecules/FilterOptions.tsx
+++ src/app/components/molecules/FilterOptions.tsx
@@ -1,3 +1,4 @@
+import AccordionGroup from "./AccordionGroup";
 import { FilterProp } from "./models/FilterProp.Model";
 
 type Props<T> = {
@@ -8,58 +9,64 @@ type Props<T> = {
     closePopup?: () => void;
 };
 
-const FilterOptions = <T,>({ filters, props, setFilters, onReset, closePopup } : Props<T>) => {
-    return (
-        <div className="card shadow-lg rounded-4" style={{  minWidth: 340 }}>
-            <div className="card-body p-4">
-                <div className="fw-bold fs-5 mb-3">Filter Options</div>
-                <div style={{ maxHeight: '38rem', overflow: 'auto' }}>
-                    { props.map((p, i) => (
-                        <div key={i} className="mb-3">
-                            <label className="form-label"> {p.label} </label>
+const FilterOptions = <T,>({ filters, props, setFilters, onReset, closePopup } : Props<T>) => (
+    <div className="filter-popup card shadow-lg rounded-4" style={{  minWidth: 340 }}>
+        <div className="card-body p-4">
+            <div className="fw-bold fs-5 mb-3">Filter Options</div>
+            <div style={{ maxHeight: '38rem', overflow: 'auto' }}>
+                { props.filter(p => p.type === "text" || p.type === "date").map((p, i) => (
+                    <div key={i} className="mb-3">
+                        <label className="form-label"> {p.label} </label>
+                        { p.type == "text" ?
+                            <input type="text" className="form-control" value={(filters as Record<string, string>)[p.key]} onChange={(e) => setFilters({...filters, [p.key]: e.target.value})}/>
+                            :
+                            <input type="date" className="form-control" value={(filters as Record<string, string>)[p.key]} onChange={(e) => setFilters({...filters, [p.key]: e.target.value})}/>
+                        }
+                    </div>
+                ))}
+
+                <div className="filter-selectors">
+                    <AccordionGroup items={
+                        props.filter(p => Array.isArray(p.type) && p.type.every(option => typeof option === "string") && p.type.length > 0).map((p) => (
                             {
-                                p.type === 'text' ?
-                                    <input type="text" className="form-control" value={(filters as Record<string, string>)[p.key]} onChange={(e) => setFilters({...filters, [p.key]: e.target.value})}/>
-                                : p.type === 'date' ?
-                                    <input type="date" className="form-control" value={(filters as Record<string, string>)[p.key]} onChange={(e) => setFilters({...filters, [p.key]: e.target.value})}/>
-                                :
-                                    (p.type.map(cbox => (
-                                        <div key={cbox+i} className="form-check form-check-custom form-check-solid mb-3">
-                                            <input 
-                                                type="checkbox" 
-                                                className="form-check-input" 
-                                                id={`${cbox}_cbox`}
-                                                checked={(filters as Record<string, string[]>)[p.key]?.includes(cbox)}
-                                                onChange={() => 
-                                                    setFilters({
-                                                        ...filters, 
-                                                        [p.key]: 
-                                                        ((filters as Record<string, string[]>)[p.key].includes(cbox) ? 
-                                                            (filters as Record<string, string[]>)[p.key].filter(v => v !== cbox)
-                                                            : [...((filters as Record<string, string[]>)[p.key] || []), cbox]
-                                                        )})
-                                                }
-                                            />
-                                            <label className="form-check-label" htmlFor={`${cbox}_cbox`}>
-                                                {cbox}
-                                            </label>
-                                        </div>
-                                    )))
+                                title: p.label,
+                                children: (p.type as string[]).map((cbox, i) => (
+                                    <div key={cbox+i} className="form-check form-check-custom form-check-solid mb-3">
+                                        <input 
+                                            type="checkbox" 
+                                            className="form-check-input" 
+                                            id={`${cbox}_cbox`}
+                                            checked={(filters as Record<string, string[]>)[p.key]?.includes(cbox)}
+                                            onChange={() => 
+                                                setFilters({
+                                                    ...filters, 
+                                                    [p.key]: 
+                                                    ((filters as Record<string, string[]>)[p.key].includes(cbox) ? 
+                                                        (filters as Record<string, string[]>)[p.key].filter(v => v !== cbox)
+                                                        : [...((filters as Record<string, string[]>)[p.key] || []), cbox]
+                                                    )})
+                                            }
+                                        />
+                                        <label className="form-check-label" htmlFor={`${cbox}_cbox`}>
+                                            {cbox}
+                                        </label>
+                                    </div>
+                                ))
                             }
-                        </div>
-                    ))}
-                </div>
-                <div className="d-flex justify-content-between">
-                    <button type="button" className="btn btn-light" onClick={onReset}>
-                        Reset
-                    </button>
-                    <button type="button" className="btn btn-dark" onClick={closePopup}>
-                        Apply
-                    </button>
+                        ))
+                    }/>
                 </div>
             </div>
+            <div className="d-flex justify-content-between">
+                <button type="button" className="btn btn-light" onClick={onReset}>
+                    Reset
+                </button>
+                <button type="button" className="btn btn-dark" onClick={closePopup}>
+                    Apply
+                </button>
+            </div>
         </div>
-    )
-}
+    </div>
+)
 
 export default FilterOptions;
