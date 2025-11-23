5f06e211d354a56069856a9cd6f3737fae625fd2
Carlos Montoya <98224572+CarlosMontoya17@users.noreply.github.com>
2025-11-23 02:15:02 -0600

    ⚒️ build url image fn

--- src/app/services/utilsService.ts
+++ src/app/services/utilsService.ts
@@ -8,7 +8,13 @@ export const filterData = (data: any[], filters: Record<string, string | string[
     data.filter(d => Object.keys(filters).every(k => {
         const _filterValue = filters[k]; 
         if(!_filterValue || _filterValue.length===0) return true;
-        return typeof _filterValue == "string" ? 
+        return typeof _filterValue == "string" && d[k]? 
             normalizeStr(d[k]).toUpperCase().includes(normalizeStr(_filterValue).toUpperCase()) 
             : (_filterValue.includes(normalizeStr(d[k]))) 
     }));
+
+
+export const getURLImage = (path: string) => {
+    const u = new URL(process.env.REACT_APP_API_URL??'');
+    return `${u.protocol}//${u.host}${path}`;
+}
