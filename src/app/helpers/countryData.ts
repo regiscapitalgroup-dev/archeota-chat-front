d2c0902fba4de2b53eeb3b03985b55bd6d04f733
Carlos Montoya <98224572+CarlosMontoya17@users.noreply.github.com>
2025-11-27 13:20:18 -0600

    🩹 minimal fixes
    - remove US duplicated
    - add CLIENT on user roles
    - return generic object on countrydata

--- src/app/helpers/countryData.ts
+++ src/app/helpers/countryData.ts
@@ -1,6 +1,8 @@
 import countries from "../constants/countries"
 
 export const getCountryData = (country: string) => {
+    if(!country)
+        return { code: '', country: 'N/A', flag: '', phoneCode: '' }
     return countries.find(c => c.country.includes(country) || country.includes(c.country)) ?? { code: '', country: 'N/A', flag: '', phoneCode: '' }
 }
 
