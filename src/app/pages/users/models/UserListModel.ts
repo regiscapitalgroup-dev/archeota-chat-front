a4750afa4f8df85c1d6ef657b5e6bdcd847f2b8d
Carlos Montoya <98224572+CarlosMontoya17@users.noreply.github.com>
2025-12-03 23:16:19 -0600

    🩹 fix user model

--- src/app/pages/users/models/UserListModel.ts
+++ src/app/pages/users/models/UserListModel.ts
@@ -5,9 +5,11 @@ export interface UserListModel {
     lastName: string;
     email: string;
     role: string;
+    roleCode: string;
     dependentsCount: number;
     isActive: boolean;
     classification: string | null;
     color: string | null;
     company: string | null;
+    companyId: number | null;
 }
